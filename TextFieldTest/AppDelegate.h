de2fe36940d7f91485606be3da2fcee5e3d3e68d
Saori Kohno <saorikohno@yahoo.com>
2016-03-07 14:20:13 +0900

    Initial Commit

---
+++ TextFieldTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextFieldTest
+//
+//  Created by 河野 さおり on 2016/03/07.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TextFieldTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TextFieldTest
+//
+//  Created by 河野 さおり on 2016/03/07.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
