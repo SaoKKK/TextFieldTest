d2ab3c6362e647249283785b5acc43c61a31cccc
Saori Kohno <saorikohno@yahoo.com>
2016-03-07 16:16:39 +0900

    finish

--- TextFieldTest/AppDelegate.m
+++ TextFieldTest/AppDelegate.m
@@ -13,7 +13,9 @@
 @property (weak) IBOutlet NSWindow *window;
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate{
+    IBOutlet NSTextField *_txtField;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
@@ -21,6 +23,14 @@
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (IBAction)pshPartOfText:(id)sender {
+   NSMutableAttributedString *text = [[_txtField attributedStringValue] mutableCopy];
+   NSDictionary *attr = @{NSFontAttributeName:[NSFont systemFontOfSize:11 weight:NSFontWeightBold]};
+   NSRange range = [text.string rangeOfString:@"the font"];
+   [text setAttributes:attr range:range];
+   [_txtField setAttributedStringValue:text];
 }
 
 @end
